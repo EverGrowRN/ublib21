7f81406f5a3e247ba21e65972e261ddbcd531716
Natalia Pulido <jenapule@gmail.com>
2024-03-20 10:33:11 -0300

    created BinaryObjectType and GraphicType

--- src/ublib21/common/udt.py
+++ src/ublib21/common/udt.py
@@ -6,7 +6,7 @@ CCTS 2.01 http://www.unece.org/cefact/ebxml/CCTS_V2-01_Final.pdf tables
 """
 
 from ublib21.common.ccts_cct import AmountType as AmountType_
-
+from ublib21.common.ccts_cct import BinaryObjectType as BinaryObjectType_
 
 class AmountType(AmountType_):
     """
@@ -14,3 +14,19 @@ class AmountType(AmountType_):
     """
     def __init__(self, value: float, currency_id: str, currency_code_list_version_id: str = None):
         super().__init__(value, currency_id, currency_code_list_version_id)
+
+
+class BinaryObjectType(BinaryObjectType_):
+    """
+    A set of finite-length sequences of binary octets.
+    """
+    def __init__(self, value, mime_code, format_=None, encoding=None, 
+                 character_set_code=None, uri=None, filename=None):
+        super().__init__(value, mime_code, format_,  encoding, character_set_code, uri, filename)
+
+
+class GraphicType(BinaryObjectType):
+    """
+    A diagram, graph, mathematical curve, or similar representation.
+    """
+    pass
