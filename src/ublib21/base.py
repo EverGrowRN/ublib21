4e68af79b72ee0bbf4f5c8b39582c4eefd7b9ef4
Ramiro Detke <ramiro.fer.det@gmail.com>
2024-03-20 10:24:54 -0300

    Update tag building strategy

--- src/ublib21/base.py
+++ src/ublib21/base.py
@@ -9,7 +9,7 @@ class MissingAttributeNamesMappingException(Exception):
 
 class UBLParseableObject:
     prefix = None
-    tag = ''
+    tag = None
     attr_names_mapping = None
 
     def __init__(self, value) -> None:
@@ -27,8 +27,19 @@ class UBLParseableObject:
         attr_string = ''
         for attr, string_attr in self.attr_names_mapping.items():
             value = getattr(self,attr,'')
-            attr_string += value and f' {string_attr}={value}' or ''
+            attr_string += value and f' {string_attr}="{value}"' or ''
         return attr_string
     
+    def get_name(self):
+        # If a tag was set, return that
+        if self.tag is not None:
+            return self.tag
+        # Otherwise, build a tag from class name
+        if self.__class__.__name__.endswith('Type'):
+            # Removes 'Type' from class name
+            return self.__class__.__name__[:-4]
+        
+        return self.__class__.__name__
+    
     def get_tag(self):
-        return f'{self.prefix}:{self.tag}' if self.prefix else f'{self.tag}'
+        return f'{self.prefix}:{self.get_name()}' if self.prefix else f'{self.get_name()}'
