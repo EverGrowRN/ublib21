2aeafea1b8b51d5ba5e57ab590ce4e03fef33d67
Natalia Pulido <jenapule@gmail.com>
2024-03-29 11:41:54 -0300

    finished the debit_note

--- src/ublib21/documents/debit_note.py
+++ src/ublib21/documents/debit_note.py
@@ -122,8 +122,7 @@ class Debit_note(Document, ComplexXMLParseableObject):
                     accountingsupplier_party:cac.AccountingSupplierParty,
                     accounting_customer_party:cac.AccountingCustomerParty,
                     requested_monetary_total:cac.RequestedMonetaryTotal,
-                    debit_note_line:List[cac.DebitNoteLine], 
-                    
+                    debit_note_line:List[cac.DebitNoteLine],
                     ubl_extensions:ext.UBLExtensions = None,
                     ubl_version_id:cbc.UBLVersionID = None,
                     customization_id:cbc.CustomizationID = None,
@@ -167,5 +166,66 @@ class Debit_note(Document, ComplexXMLParseableObject):
                     payment_exchange_rate:cac.PaymentExchangeRate =  None,
                     payment_alternative_exchange_rate:cac.PaymentAlternativeExchangeRate =  None,
                     tax_total:List[cac.TaxTotal] =  None) -> None:
-          self.id_ = id_
+            self.id_ = id_
+            issue_date =  issue_date
+            accountingsupplier_party = accountingsupplier_party
+            accounting_customer_party = accounting_customer_party
+            requested_monetary_total =  requested_monetary_total
+            debit_note_line =  debit_note_line
+            ubl_extensions =  ubl_extensions
+            ubl_version_id =  ubl_version_id
+            customization_id = customization_id
+            profile_id = profile_id
+            profile_execution_id = profile_execution_id
+            copy_indicator = copy_indicator
+            uuid = uuid
+            issue_time = issue_time
+            Note = Note
+            tax_point_date = tax_point_date
+            document_currency_code = document_currency_code
+            tax_currency_code = tax_currency_code
+            pricing_currency_code = pricing_currency_code
+            payment_currency_code = payment_currency_code
+            payment_alternative_currency_code = payment_alternative_currency_code
+            accounting_cost_code = accounting_cost_code
+            accounting_cost = accounting_cost
+            line_count_numeric = line_count_numeric
+            invoice_period = invoice_period
+            discrepancy_response = discrepancy_response
+            order_reference = order_reference
+            billing_reference = billing_reference
+            despatch_document_reference = despatch_document_reference
+            receipt_document_reference = receipt_document_reference
+            statement_document_reference = statement_document_reference
+            contract_document_reference = contract_document_reference
+            additional_document_reference = additional_document_reference
+            signature = signature
+            payee_party = payee_party
+            buyer_customer_party = buyer_customer_party
+            seller_supplier_party = seller_supplier_party
+            tax_representative_party = tax_representative_party
+            prepaid_payment = prepaid_payment
+            allowance_charge = allowance_charge
+            delivery = delivery
+            delivery_terms = delivery_terms
+            payment_means = payment_means
+            payment_terms = payment_terms
+            tax_exchange_rate = tax_exchange_rate
+            pricing_exchange_rate = pricing_exchange_rate
+            payment_exchange_rate = payment_exchange_rate
+            payment_alternative_exchange_rate = payment_alternative_exchange_rate
+            tax_total = tax_total
+
+    def __str__(self):
+        tag = self.get_tag()
+        namespaces = ''
+        if self.xml_namespaces is not None:
+            for ns in self.xml_namespaces:
+                namespaces += f' {ns}'
+
+        return f'<{tag}{namespaces}>{self.get_value()}</{tag}>'
+
+    def to_file(self):
+        full_file = f'<?xml version="1.0" encoding="UTF-8" standalone="no"?>{self}'
+        return full_file
         
